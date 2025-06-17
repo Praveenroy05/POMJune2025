664f1f8fb359e467883b8acae43cc6fba097ac43
Praveen <praveen.k@xnara.com>
2025-06-17 22:51:29 +0530

    Added github

---
+++ tests/github.spec.ts
@@ -0,0 +1,29 @@
+// github , bitbucket
+
+// If you are the one who is developing the framework from scratch
+// 1. Write the test case and create a repository inside github or bitbucket
+// 2. Initliase the git inside your local system - git init
+// 3. Add the test case to the repository - (git add .) - git add pathofthefile
+// 4. Commit the test case - git commit -m "commit message"
+// 5. Create a branch inside the repo - git branch -M master
+// 6. Connect with the repository to your project inside the local -git remote add origin https://github.com/Praveenroy05/TSDummy.git
+// 7. Push the code to the repository - git push -u origin master
+
+
+// To check the branches - git branch
+// Any new feature - 
+// 0. Move to the master branch - git checkout master - git pull (It will pull all the changes from the remote git to your local git)
+// 1. Create a new branch - git checkout -b featurebranchname
+// 2. Make the changes - git add . 
+// 3. Commit the changes - git commit -m "commit message"
+// 4. Push the changes to the remote repository - git push -u origin featurebranchnam
+
+
+// If the framework is already developed and repository are also available
+// 1. clone the repository inside your local system
+// 2. Create a new branch - git checkout -b featurebranchname
+// 3. Make the changes - git add . 
+// 4. Commit the changes - git commit -m "commit message"
+// 5. Push the changes to the remote repository - git push -u origin featurebranchnam
+
+
